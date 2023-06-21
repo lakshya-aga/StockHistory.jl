8df6a980c3e32ebe858c32433eedb25fe6ed23c5
lakshya-aga <lakshya001@e.ntu.edu.sg>
2023-06-21 16:57:13 +0800

    Increased Dependency List

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,6 @@ using Test
 
 @testset "EconomicHistory.jl" begin
     # Write your tests here.
-    @test EconomicHistory.greet_your_package_name() == "Hello EconomicHistory"
-    @test EconomicHistory.greet_your_package_name() != "Hello world!"
+    @test EconomicHistory.greet_economicHistory() == "Hello EconomicHistory"
+    @test EconomicHistory.greet_economicHistory() != "Hello world!"
 end
